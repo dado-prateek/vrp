8454ca6b37092e22f2d0c28d2b07475873e9200b
Gregory Borg <q.borg@yandex.ru>
2016-03-24 01:10:42 +0300

    refactoring

--- grabber.py
+++ grabber.py
@@ -21,7 +21,7 @@ DETAIL_PAGES_XPATH = '//a[@class="w-portfolio-item-anchor"]/attribute::href'
 HIGH_XPATH = '//td[a="3200×1600 High"]/a/attribute::href'
 ANDROID_XPATH = '//td[a="1080p"]/a/attribute::href'
 
-formats_xpath = (HIGH_XPATH, ANDROID_XPATH)
+FORMAT_XPATHS = (HIGH_XPATH, ANDROID_XPATH)
 
 
 log_format = '%(asctime)16s %(levelname)6s %(message)s'
@@ -31,11 +31,43 @@ logging.basicConfig(format=log_format,
 console = logging.StreamHandler()
 console.setFormatter(logging.Formatter(log_format))
 logging.getLogger('').addHandler(console)
-
-
 log = logging.getLogger(__name__)
 
 
+def main():
+    cookies = json.load(open(COOKIES_JSON_PATH))
+    page = with_retry(requests.get, VIDEOS_URL, cookies=cookies, timeout=TIMEOUT)
+    tree = html.fromstring(page.content)
+    detail_pages = tree.xpath(DETAIL_PAGES_XPATH)
+
+    files_urls = []
+    for url in detail_pages:
+        time.sleep(random.random() + 2)
+        info = get_video_page_info(url, cookies)
+        for video_url in info['video_urls']:
+            with_retry(download_file, video_url, cookies)
+            files_urls.append(video_url)
+
+    with open("log/urls-{}.json".format(datetime.datetime.now().isoformat()), 'w') as f:
+        json.dump(files_urls, f)
+
+
+def get_video_page_info(page_url, cookies):
+    info = {
+        'video_urls': []
+    }
+    
+    try:
+        page = with_retry(requests.get, page_url, cookies=cookies, timeout=TIMEOUT)
+        tree = html.fromstring(page.content)
+        for xpath in FORMAT_XPATHS:
+            info['video_urls'].append(tree.xpath(xpath)[0])
+    except:
+        log.exception('Unhandled error on page {}'.format(page_url))
+
+    return info
+
+        
 def download_file(url, cookies=None):
     log.info('Downloading file: {}'.format(url))
     fp = os.path.join(DOWNLOAD_PATH, url.split('/')[-1])
@@ -66,30 +98,7 @@ def with_retry(func, *args, **kwargs):
             log.info('Retrying ({})'.format(retry_count))
             
     raise Exception('MAX_RETRY count exeeded')
-        
-
-def main():
-    cookies = json.load(open(COOKIES_JSON_PATH))
-    page = with_retry(requests.get, VIDEOS_URL, cookies=cookies, timeout=TIMEOUT)
-    tree = html.fromstring(page.content)
-    detail_pages = tree.xpath(DETAIL_PAGES_XPATH)
-
-    files_urls = []
-    for url in detail_pages:
-        time.sleep(random.random() + 2)
-        try:
-            page = with_retry(requests.get, url, cookies=cookies, timeout=TIMEOUT)
-            tree = html.fromstring(page.content)
-            for xpath in formats_xpath:
-                video_url = tree.xpath(xpath)[0]
-                files_urls.append(video_url)
-                with_retry(download_file, video_url, cookies)
-        except:
-            log.exception('Unhandled error on page {}'.format(url))
-
-    with open("log/urls-{}.json".format(datetime.datetime.now().isoformat()), 'w') as f:
-        json.dump(files_urls, f)
-              
+                      
 
 if __name__ == '__main__':
     main()
